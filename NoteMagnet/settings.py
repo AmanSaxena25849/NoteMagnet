7c0da803857eea9c4aafc502e95b4fefee42e3af
Aman Saxena <aman25849@gmail.com>
2025-04-10 23:40:22 +0530

    added the home page view and template for it.

--- NoteMagnet/settings.py
+++ NoteMagnet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h(g0@n#&-j0cs5=7-nzjn!l_=e%s-3pohpwlz@^%ldyyb#p^49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
