a49a5f6c59cb882d183f0ec24754ed8325823ce6
Aman Saxena <aman25849@gmail.com>
2025-04-16 18:10:33 +0530

    add allauth and add signup page.
    1. add allauth settings in settings.py.
    2. add custom model for signup.
    3. add a custom form for signup.
    4. add a signup html template for signup.
    5. removed urls.py from accounts app.
    6. made migration to the database.

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ from django.db.models import JSONField
 
 class users(AbstractUser):
     age = models.PositiveIntegerField(validators=[MinValueValidator(9), MaxValueValidator(110)], null=True)
-    phone_number = PhoneNumberField(unique=True, blank=True, null=True)
+    phone_number = PhoneNumberField(max_length=20, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     social_links = models.JSONField(blank=True, null=True)
     notifications = models.BooleanField(default=False)
--- accounts/views.py
+++ accounts/views.py
@@ -3,21 +3,4 @@ from .forms import SignupForm
 
 # Create your views here.
 
-def signup(request):
-    if request.method == "POST":
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data['username'])
-            form.save()
-            return redirect("redirect")
-        else:
-            print(form.errors)
-    else:
-        form = SignupForm()
-        
-    return render(request, "accounts/signup.html", {"form": form})
- 
-
-def redirect_page(request):
-    return render(request, "accounts/redirect.html")
     
